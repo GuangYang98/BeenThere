600a401f59a5f42c7c9ca4d63a7fb3c586fa3670
NikolaLiu <NikolaLiu@icloud.com>
2020-03-09 15:33:32 +0800

    Menu bar completed

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,25 +1,28 @@
 <template>
-  <div class="text-center" style="margin-right: 20px;">
-    <v-menu>
-      <template v-slot:activator="{ on }">
-        <v-btn
-          color="blue"
-          v-on="on"
+  <div class="text-center" style="margin-right: 20px;" data-app="true">
+    <v-menu
+          transition="slide-y-transition"
+          bottom
         >
-		<v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </template>
-
-      <v-list>
-        <v-list-item
-          v-for="(item, index) in items"
-          :key="index"
-          @click="jump"
-        >
-          <v-list-item-title>{{ item.title }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
+          <template v-slot:activator="{ on }">
+            <v-btn
+              class="purple"
+              style="background-color: transparent;"
+              v-on="on"
+            >
+				<v-icon xx-large color="purple darken-2">mdi-dialpad</v-icon>
+            </v-btn>
+          </template>
+          <v-list style="background-color: dodgerblue; font-weight: 700;">
+            <v-list-item
+              v-for="(item, i) in items"
+              :key="i"
+              @click="jump(i)"
+            >
+              <v-list-item-title style="color: white;">{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
   </div>
 </template>
 
@@ -28,20 +31,19 @@ export default {
 	name: "Menu",
 	data: () => ({
 		items: [
-		{ title: 'Join' },
+		{ title: '主页' },
 		{ title: 'Beenthere 大家庭' },
-		{ title: 'Click Me' },
-		{ title: 'Click Me 2' },
+		{ title: '我要加入' },
 	],
 	}),
 	methods:{
 		jump: function(keyword){
-			if(keyword == "join")
+			if(keyword == "我要加入")
 				window.location.href="https://beentherebuddy.com/711943786725";
-			else if(keyword == "home")
+			else if(keyword == "主页")
 				window.location.href="https://beentherebuddy.com/";
 			else
-				window.location.href="https://beentherebuddy.com//beenthere-2";
+				window.location.href="https://beentherebuddy.com/beenthere-2";
 		}
 	}
 }
